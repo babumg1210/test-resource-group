c351be9fdd57e49e136c9ec02ca24fef217c0aeb
babumg1210 <babumg1210@outlook.com>
2025-12-07 09:23:02 +0530

    Refactor network resources in main.tf

--- projects/modules/network/main.tf
+++ projects/modules/network/main.tf
@@ -1,15 +1,15 @@
 resource "azurerm_virtual_network" "vnet" {
   name                = "${var.project}-${var.env}-vnet"
-  address_space       = var.address_space
   location            = var.location
   resource_group_name = var.resource_group_name
+  address_space       = [var.address_space]
 }
 
 resource "azurerm_subnet" "subnets" {
-  for_each = var.subnets
+  for_each = { for s in var.subnets : s.name => s }
 
-  name                 = each.key
+  name                 = each.value.name
   resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = [each.value]
+  address_prefixes     = [each.value.address_prefix]
 }
