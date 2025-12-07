cc873f6e6f966ee8988c31e4cfd59f2baaf97002
babumg1210 <babumg1210@outlook.com>
2025-12-07 09:18:57 +0530

    Update main.tf

--- projects/enviroments/dev/main.tf
+++ projects/enviroments/dev/main.tf
@@ -1,10 +1,10 @@
 provider "azurerm" {
   features {}
 
-  subscription_id = "0615a8ec-9240-4666-bd0e-6dde52ce08a9"
-  tenant_id       = "02495ddd-54e3-42e8-ad3a-d1f2091f0034"
-  client_id       = "4beb6248-b0e2-4cbb-b43c-a48805c49d98"
-  client_secret   = "uXD8Q~OfWCLU~JSN9ZSN55KBD9Ix3oGhuVIrwaZl"
+#  subscription_id = "0615a8ec-9240-4666-bd0e-6dde52ce08a9"
+#  tenant_id       = "02495ddd-54e3-42e8-ad3a-d1f2091f0034"
+#  client_id       = "4beb6248-b0e2-4cbb-b43c-a48805c49d98"
+#   client_secret   = "uXD8Q~OfWCLU~JSN9ZSN55KBD9Ix3oGhuVIrwaZl"
 }
 
 module "rg" {
@@ -23,3 +23,4 @@ module "network" {
   subnets             = var.subnets
   resource_group_name = module.rg.name
 }
+
