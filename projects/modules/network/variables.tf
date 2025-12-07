26a964d9f4e7196c2ce1e318938637987db9a063
babumg1210 <babumg1210@outlook.com>
2025-12-07 09:23:29 +0530

    Refactor subnets variable to use list of objects
    Updated the 'subnets' variable type to a list of objects with 'name' and 'address_prefix' attributes.

--- projects/modules/network/variables.tf
+++ projects/modules/network/variables.tf
@@ -1,8 +1,11 @@
 variable "project" {}
 variable "env" {}
 variable "location" {}
+variable "resource_group_name" {}
 variable "address_space" {}
 variable "subnets" {
-  type = map(string)
+  type = list(object({
+    name           = string
+    address_prefix = string
+  }))
 }
-variable "resource_group_name" {}
