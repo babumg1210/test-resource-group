5c7943a5adee9c16cbddff16a5b7fa5fc01d7591
babumg1210 <babumg1210@outlook.com>
2025-12-07 09:33:18 +0530

    Add Terraform backend configuration for production

---
+++ projects/enviroments/production/backend.tf
@@ -0,0 +1,7 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name   = "storage-ac1"
+    storage_account_name  = "testterraformaccount123"
+    container_name        = "continaer"
+    key                   = "production.tfstate"
+}
