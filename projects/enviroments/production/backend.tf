6769e2a990d216a2f0b1247f5d246f7fc43900d4
babumg1210 <babumg1210@outlook.com>
2025-12-08 18:39:24 +0530

    Update backend.tf

--- projects/enviroments/production/backend.tf
+++ projects/enviroments/production/backend.tf
@@ -3,7 +3,7 @@ terraform {
     resource_group_name  = "storage-ac1"
     storage_account_name = "testterraformaccount123"
     container_name       = "terraform"
-    key                  = "terraform.tfstate"
+    key                  = "production.tfstate"
   }
 
   required_providers {
