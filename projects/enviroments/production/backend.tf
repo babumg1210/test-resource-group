f2e7139e450f7531717e7030ee817322789513a1
babumg1210 <babumg1210@outlook.com>
2025-12-08 18:38:43 +0530

    Update backend.tf

--- projects/enviroments/production/backend.tf
+++ projects/enviroments/production/backend.tf
@@ -1,7 +1,19 @@
 terraform {
   backend "azurerm" {
-    resource_group_name   = "storage-ac1"
-    storage_account_name  = "testterraformaccount123"
-    container_name        = "continaer"
-    key                   = "production.tfstate"
+    resource_group_name  = "storage-ac1"
+    storage_account_name = "testterraformaccount123"
+    container_name       = "terraform"
+    key                  = "terraform.tfstate"
+  }
+
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
 }
